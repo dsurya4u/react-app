19184d5d531c78784fa314d3ee696b34fc9af207
Surya D <55309500+dsurya4u@users.noreply.github.com>
2025-02-27 22:05:12 +0530

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #installing java:
-apt-get update
-apt-get install -y openjdk-11-jre
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre
+java -version
+openjdk version "17.0.13" 2024-10-15
+OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
+OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 
 #installing docker:
 apt-get update
@@ -11,11 +15,11 @@ apt-get install -y docker.io
 #installing jenkins:
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y jenkins
+sudo apt-get install jenkins
 
 #checking the installed services:
 echo "This is the Java package - "
